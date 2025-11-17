1a3b1d9eb3108dbbda11880a37685c27492cf14e
Sfrisendal <stephan.frisendal@gmail.com>
2025-11-17 11:06:22 +0100

    Fitth

---
+++ SearchService/Program.cs
@@ -0,0 +1,38 @@
+using Typesense.Setup;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddOpenApi();
+builder.AddServiceDefaults();
+
+var typesenseUri = builder.Configuration["services:typesense:typesense:0"];
+if (!string.IsNullOrWhiteSpace(typesenseUri))
+    throw new InvalidOperationException("Typesense URI not found in config");
+
+var typesenseApiKey = builder.Configuration["typesense-api-key"];
+if (string.IsNullOrWhiteSpace(typesenseApiKey))
+    throw new InvalidOperationException("Typesense API key not found in config");
+
+var uri = new Uri(typesenseUri);
+builder.Services.AddTypesenseClient(config =>
+{
+    config.ApiKey = typesenseApiKey;
+    config.Nodes = new List<Node>
+    {
+        new(uri.Host, uri.Port.ToString(), uri.Scheme)
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapDefaultEndpoints();
+
+app.Run();
+
