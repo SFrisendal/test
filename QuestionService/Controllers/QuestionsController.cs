5b625e7b8c179de763c406a1be0e49e62f84c8bc
Sfrisendal <stephan.frisendal@gmail.com>
2025-11-17 10:08:36 +0100

    Third Commit

--- QuestionService/Controllers/QuestionsController.cs
+++ QuestionService/Controllers/QuestionsController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using QuestionService.Data;
 using QuestionService.Models;
 using QuestionService.DTOs;
@@ -15,6 +16,12 @@ public class QuestionsController(QuestionDbContext db) : ControllerBase
     [HttpPost]
     public async Task<ActionResult<Question>> CreateQuestion(CreateQuestionDto dto)
     {
+        var validTags = await db.Tags.Where(x => dto.Tags.Contains(x.Slug)).ToListAsync();
+        
+        var missing = dto.Tags.Except(validTags.Select(x => x.Slug).ToList()).ToList();
+        
+        if (missing.Count != 0 ) return BadRequest($"The provided tags are invalid. Invalid tags: {string.Join(", ", missing)}");
+        
         var userid = User.FindFirstValue(ClaimTypes.NameIdentifier);
         var userName = User.FindFirstValue("name");
 
@@ -33,5 +40,71 @@ public class QuestionsController(QuestionDbContext db) : ControllerBase
         await db.SaveChangesAsync();
         
         return Created($"questions/{question.Id}", question);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Question>>> GetQuestions(string? tag)
+    {
+        var query = db.Questions.AsQueryable();
+
+        if (!string.IsNullOrEmpty(tag))
+        {
+            query = query.Where(x => x.TagSlugs.Contains(tag));
+        }
+        
+        return await query.OrderByDescending(x => x.CreatedAt).ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Question>> GetQuestion(string id)
+    {
+        var question = await db.Questions.FindAsync(id);
+        if (question is null) return NotFound();
+
+        await db.Questions.Where(x => x.Id == question.Id)
+            .ExecuteUpdateAsync(setters => setters.SetProperty(x => x.ViewCount,
+                x => x.ViewCount + 1));
+        
+        return question;
+    }
+
+    [Authorize]
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateQuestion(string id, CreateQuestionDto dto)
+    {
+        var question = await db.Questions.FindAsync(id);
+        if (question is null) return NotFound();
+
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId != question.AskerId) return Forbid();
+        
+        var validTags = await db.Tags.Where(x => dto.Tags.Contains(x.Slug)).ToListAsync();
+        
+        var missing = dto.Tags.Except(validTags.Select(x => x.Slug).ToList()).ToList();
+        
+        if (missing.Count != 0 ) return BadRequest($"The provided tags are invalid. Invalid tags: {string.Join(", ", missing)}");
+
+        question.Title = dto.Title;
+        question.Content = dto.Content;
+        question.TagSlugs = dto.Tags;
+        question.UpdatedAt = DateTime.UtcNow;
+
+        await db.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [Authorize]
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteQuestion(string id, CreateQuestionDto dto)
+    {
+        var question = await db.Questions.FindAsync(id);
+        if (question is null) return NotFound();
+
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId != question.AskerId) return Forbid();
+        
+        db.Questions.Remove(question);
+        await db.SaveChangesAsync();
+        return NoContent();
     }
 }
---
+++ QuestionService/Validators/TagListValidator.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuestionService.Validators;
+
+public class TagListValidator (int min, int max) : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is List<string> tags)
+        {
+            if (tags.Count >= min && tags.Count <= max) return ValidationResult.Success;
+        }
+        
+        return new ValidationResult($"A minimum of {min} and a maximum of {max} of tags must be specified.");
+    }
+}
